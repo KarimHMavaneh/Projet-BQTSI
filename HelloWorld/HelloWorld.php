658b878291a49934218078168dc852dadebaf555
Me <thonin@ok>
2023-03-02 15:58:42 +0100

    - Début du projet - Creation de HelloWorld - Creation Branche : "feature_init"

---
+++ HelloWorld/HelloWorld.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <?php
+        echo"<h1><center>HelloWorld</center></h1>";
+    ?>
+</body>
+</html>
